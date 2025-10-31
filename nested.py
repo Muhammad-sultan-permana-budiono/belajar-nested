e7368c11e517f1f431dedd959acbac170b0243c6
muhammad sultan <budionosultan8@gmail.com>
2025-10-31 08:12:39 +0700

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,11 @@
+max = int(input("jumlah bintang:"))
+for i in range (max):
+    for j in range (i):
+        print("*", end=" ")
+    print()
+
+max = int(input("jumlah bintang:"))
+for i in range (max):
+    for j in range (i):
+        print("*", end=" ")
+    print()
